86014ae9aa8fe91410747f78f52fd7575e7391f7
katawinabluu <eahamor@gmail.com>
2025-04-08 13:06:40 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,17 @@
 function checkAnswers() {
     // Correct answers
     const correctAnswers = {
-        q1: "C", // The dog chased the ball. 
+        q1: "C", // The dog chased the ball.
+        q2: "B", // William Shakespeare
+        q3: "B"  // Jupiter
     };
 
     // Get the user's answers
     const form = document.getElementById('quizForm');
     const userAnswers = {
         q1: form.q1.value,
+        q2: form.q2.value,
+        q3: form.q3.value
     };
 
     let score = 0;
